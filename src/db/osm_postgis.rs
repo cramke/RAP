7fc2e32e30efeb52df35822ea68d954dbf46f41f
cramke <carsten.ramke@gmail.com>
2022-12-05 21:11:43 +0100

    [Postgis] Split into functions

--- src/db/osm_postgis.rs
+++ src/db/osm_postgis.rs
@@ -1,29 +1,44 @@
-use sqlx::postgres::{PgPoolOptions, PgRow};
-use sqlx::{FromRow, Row};
+use sqlx::postgres::{PgPoolOptions, PgRow, PgPool};
+use sqlx::{FromRow, Row, Postgres, Pool};
+use futures::executor::block_on;
 
 pub fn tester() {
     println!("Yeat");
 }
 
 pub struct OSMPostgis {
-    pool: i32
+    pool: Pool<Postgres>
 }
 
-pub async fn database() {
-	println!("Run some async function.");
-    let pool = PgPoolOptions::new()
+pub async fn make_db_connection() -> OSMPostgis {
+    let db = PgPoolOptions::new()
         .max_connections(5)
         .connect("postgresql://postgres:password@localhost:5432/osm")
         .await
         .unwrap();
+    return OSMPostgis{pool: db};
+}
 
+pub async fn query(pool: &Pool<Postgres>) -> Vec<PgRow> {
     let param = "0102000020E61000000300000012A5187B8AFF21408390E2D2E7D548407EC9213DA0FF21401822A7AFE7D54840AB6A381BE1FF2140ADE17DB0E7D54840";
     let rows = sqlx::query("SELECT * FROM highway_michelstadt WHERE ST_Crosses(highway_michelstadt.geometry, ($1))")
-        .bind(param)
-        .fetch_all(&pool).await.unwrap();
+    .bind(param)
+    .fetch_all(pool).await.unwrap();
+    return rows;
+}
+
+pub fn process(rows: &Vec<PgRow>) {
     for row in rows {
         let a = row.get::<String, _>("highway");
         print!("{}", a);
+    }
 }
-    println!("\nDone with asysn");
+
+pub async fn database() {
+	println!("Run some async function.");
+    let DB = make_db_connection().await;
+    let rows = query(&DB.pool).await;
+    process(&rows);
+
+    println!("\nDone with async");
 }
