99efd05c9763ed5c4cc044ff973931e201e6d08d
cramke <carsten.ramke@gmail.com>
2023-04-29 18:21:15 +0200

    Use obstructions for collision checking.

--- src/db/osm_postgis.rs
+++ src/db/osm_postgis.rs
@@ -2,14 +2,7 @@ use sqlx::postgres::PgRow;
 use sqlx::{Row, Postgres, Pool};
 
 pub async fn fetch_intersections_wkt(pool: &Pool<Postgres>, param: &str) -> Vec<PgRow> {
-    let sql = "SELECT * FROM highway_michelstadt WHERE ST_Crosses(highway_michelstadt.geometry, ST_GeomFromText(($1), 4326))";
-    sqlx::query(sql)
-        .bind(param)
-        .fetch_all(pool).await.unwrap()
-}
-
-pub async fn fetch_contains_wkt(pool: &Pool<Postgres>, param: &str) -> Vec<PgRow> {
-    let sql: &str = "SELECT * FROM obstruction_michelstadt WHERE ST_Intersects(obstruction_michelstadt.geometry, ST_GeomFromText(($1), 4326))";
+    let sql = "SELECT fclass FROM highway_michelstadt WHERE ST_Crosses(highways_hessen.geometry, ST_GeomFromText(($1), 4326))";
     sqlx::query(sql)
         .bind(param)
         .fetch_all(pool).await.unwrap()
@@ -17,7 +10,6 @@ pub async fn fetch_contains_wkt(pool: &Pool<Postgres>, param: &str) -> Vec<PgRow
 
 pub async fn fetch_intersects_wkt(pool: &Pool<Postgres>, param: &str) -> Vec<PgRow> {
     let sql: &str = "SELECT fclass FROM highways_hessen WHERE ST_Intersects(highways_hessen.geometry, ST_GeomFromText(($1), 4326))";
-    // let sql: &str = "SELECT * FROM highway_michelstadt WHERE ST_Intersects(highway_michelstadt.geometry, ST_GeomFromText(($1), 4326))";
     sqlx::query(sql).bind(param).fetch_all(pool).await.unwrap()
 }
     
--- src/osm_collision.rs
+++ src/osm_collision.rs
@@ -20,16 +20,16 @@ impl GeoCollsionChecker {
     pub async fn make_db_connection() -> Pool<Postgres> {
         const MAX_CONNECTIONS: u32 = 5;
         const URL: &str = "postgresql://postgres:password@localhost:5432/osm";
-        let db: Pool<Postgres> = PgPoolOptions::new()
+        return PgPoolOptions::new()
             .max_connections(MAX_CONNECTIONS)
             .connect(URL)
             .await
             .unwrap();
-        return db;
     }
 
-    pub async fn fetch_is_contain(&self, param: &str) -> Vec<PgRow> {
-        osm_postgis::fetch_contains_wkt(&self.pool, param).await
+    pub async fn fetch_is_intersecting(&self, param: &str) -> Vec<PgRow> {
+        let sql: &str = "SELECT name FROM obstruction_michelstadt WHERE ST_Intersects(obstruction_michelstadt.geometry, ST_GeomFromText(($1), 4326))";
+        return sqlx::query(sql).bind(param).fetch_all(&self.pool).await.unwrap();
     }
 }
 
@@ -40,13 +40,13 @@ impl CollisionChecker for GeoCollsionChecker {
     }
 
     fn is_edge_colliding(&self, start: &Point, end: &Point) -> bool {
-        let line = LineString::from(vec![*start, *end]);
-        let collisions = block_on(self.fetch_is_contain(&line.wkt_string()));
-        collisions.len() > 0 
+        let line: LineString = LineString::from(vec![*start, *end]);
+        let collisions: Vec<PgRow> = block_on(self.fetch_is_intersecting(&line.wkt_string()));
+        return collisions.len() > 0;
     }
 
     fn is_node_colliding(&self, node: &geo::Point) -> bool {
-        let collisions = block_on(self.fetch_is_contain(&node.wkt_string()));
-        collisions.len() > 0 
+        let collisions: Vec<PgRow> = block_on(self.fetch_is_intersecting(&node.wkt_string()));
+        return collisions.len() > 0;
     }
 }
